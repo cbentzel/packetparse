e57c65c805b2631dccaaa01c2490f82aab748fcf
Chris Bentzel <cbentzel@chromium.org>
2013-01-06 19:40:10 -0500

    Fix indentation

--- ae/static/pcap_parser.js
+++ ae/static/pcap_parser.js
@@ -4,52 +4,52 @@
 // http://wiki.wireshark.org/Development/LibpcapFileFormat
 
 var PcapParser = (function() {
-    'use strict';
+  'use strict';
 
-    var State = {
-        INIT: 0,
-        FILE_HEADER: 1,
-        PACKET_HEADER: 2,
-        PACKET_BODY: 3,
-        ERROR_DONE: 4,
-    };
+  var State = {
+    INIT: 0,
+    FILE_HEADER: 1,
+    PACKET_HEADER: 2,
+    PACKET_BODY: 3,
+    ERROR_DONE: 4,
+  };
 
-    var FILE_HEADER_SIZE = 24;
+  var FILE_HEADER_SIZE = 24;
 
-    function PcapParser(onFileHeader, onPacket, onError) {
-        this.error_ = undefined;
-        this.data_ = [];
-        this.totalByteCount_ = 0;
-        this.state_ = State.INIT;
-        this.onFileHeader = onFileHeader;
-        this.onPacket = onPacket;
-        this.onError = onError;
-    };
+  function PcapParser(onFileHeader, onPacket, onError) {
+    this.error_ = undefined;
+    this.data_ = [];
+    this.totalByteCount_ = 0;
+    this.state_ = State.INIT;
+    this.onFileHeader = onFileHeader;
+    this.onPacket = onPacket;
+    this.onError = onError;
+  };
 
-    PcapParser.prototype = {
-        addData: function (data) {
-            if (!(data instanceof ArrayBuffer)) {
-                throw {name: 'BadDataArg'};
-            }
+  PcapParser.prototype = {
+    addData: function (data) {
+      if (!(data instanceof ArrayBuffer)) {
+        throw {name: 'BadDataArg'};
+      }
 
-            // We always accumulate data, regardless of the
-            // state.
-            this.data_.push(data)
-            this.totalByteCount_ += data.byteLength;
+      // We always accumulate data, regardless of the
+      // state.
+      this.data_.push(data)
+      this.totalByteCount_ += data.byteLength;
 
-            // Make sure that data is an ArrayBuffer.
-            switch (this.state_) {
-            case State.INIT:
-                console.log('In INIT state');
-                if (this.totalByteCount_ >= FILE_HEADER_SIZE) {
-                    console.log('Enough data');
-                    // Parse the header. We need to copy out x bytes of data.
-                    // Perhaps have a helper for that?
-                }
-                break;
-            };
-        },
-    };
-    
-    return PcapParser;
+      // Make sure that data is an ArrayBuffer.
+      switch (this.state_) {
+      case State.INIT:
+        console.log('In INIT state');
+        if (this.totalByteCount_ >= FILE_HEADER_SIZE) {
+          console.log('Enough data');
+          // Parse the header. We need to copy out x bytes of data.
+          // Perhaps have a helper for that?
+        }
+        break;
+      };
+    },
+  };
+  
+  return PcapParser;
 })();
